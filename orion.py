40358e687094fb5bf793f2fc39381be5a5e3b7ab
Justin Leniger <jtleniger@gmail.com>
2020-01-11 17:09:43 -0700

    iso widget spinner

--- orion.py
+++ orion.py
@@ -2,9 +2,11 @@ import kivy
 kivy.require('1.11.1')
 
 from kivy.app import App
+from kivy.config import Config
 
 class OrionApp(App):
     pass
 
 if __name__ == '__main__':
+    Config.set('graphics', 'window_state', 'maximized')
     OrionApp().run()
